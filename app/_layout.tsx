0fe8c9894881faa44f7beeb3cc787a5207710912
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-12 14:08:35 -0500

    ThemeView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,11 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
-import { Text, View } from "react-native";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import ThemedView from "@/presentation/shared/ThemedView";
+import React from "react";
+import { Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../global.css";
 
 export const unstable_settings = {
@@ -15,15 +19,20 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, "background");
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <View className="bg-light-background dark:bg-dark-background">
-        <Text className="mt-10 text-3xl text-light-text dark:text-dark-text">
-          Hola mundo
-        </Text>
-      </View>
-    </ThemeProvider>
+    <GestureHandlerRootView
+      style={{ backgroundColor: backgroundColor, flex: 1 }}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text className="mt-10 text-3xl text-light-text dark:text-dark-text">
+            Hola mundo
+          </Text>
+        </ThemedView>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
