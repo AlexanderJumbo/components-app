11203243c1d40abfab66209c0ebe926a66214a00
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-12 13:43:39 -0500

    Configuración, NativeWind y paleta de colores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+import { Text, View } from "react-native";
+import "../global.css";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View className="bg-light-background dark:bg-dark-background">
+        <Text className="mt-10 text-3xl text-light-text dark:text-dark-text">
+          Hola mundo
+        </Text>
+      </View>
     </ThemeProvider>
   );
 }
