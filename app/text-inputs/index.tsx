643d5e00d40e98640fd62b6ff639ccdf04b029a1
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-18 18:59:41 -0500

    KeyboardAvoidingView

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,8 +1,9 @@
-import ThemeCard from "@/presentation/shared/ThemedCard";
+import ThemedCard from "@/presentation/shared/ThemedCard";
 import ThemedText from "@/presentation/shared/ThemedText";
 import ThemedTextInput from "@/presentation/shared/ThemedTextInput";
 import ThemedView from "@/presentation/shared/ThemedView";
 import { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 
 const TextInputsScreen = () => {
   const [form, setForm] = useState({
@@ -11,21 +12,68 @@ const TextInputsScreen = () => {
     phone: "",
   });
 
-  return (
-    <ThemedView margin>
-      <ThemeCard className="mb-5">
-        <ThemedTextInput
-          placeholder="Nombre Completo"
-          autoCapitalize="words"
-          autoCorrect={false}
-          onChangeText={(text) => setForm({ ...form, name: text })}
-        />
+  const isIOS = Platform.OS === "ios";
 
-        <ThemeCard>
-          <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
-        </ThemeCard>
-      </ThemeCard>
-    </ThemedView>
+  return (
+    <KeyboardAvoidingView behavior={isIOS ? "height" : undefined}>
+      <ScrollView>
+        <ThemedView margin>
+          <ThemedCard className="mb-5">
+            <ThemedTextInput
+              placeholder="Nombre Completo"
+              autoCapitalize="words"
+              autoCorrect={false}
+              onChangeText={(text) => setForm({ ...form, name: text })}
+            />
+            <ThemedTextInput
+              placeholder="Correo electrónico"
+              autoCorrect={false}
+              keyboardType="email-address"
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+            <ThemedTextInput
+              placeholder="Teléfono"
+              autoCorrect={false}
+              keyboardType="phone-pad"
+              onChangeText={(text) => setForm({ ...form, phone: text })}
+            />
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard className="my-2">
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard
+            style={{
+              marginBottom: isIOS ? 100 : 10,
+            }}
+          >
+            <ThemedTextInput
+              placeholder="Teléfono"
+              autoCorrect={false}
+              keyboardType="phone-pad"
+              onChangeText={(text) => setForm({ ...form, phone: text })}
+            />
+          </ThemedCard>
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
--- presentation/shared/ThemedCard.tsx
+++ presentation/shared/ThemedCard.tsx
@@ -5,14 +5,15 @@ interface Props extends ViewProps {
   className?: string;
 }
 
-const ThemeCard = ({ className, children }: Props) => {
+const ThemedCard = ({ className, children, ...rest }: Props) => {
   return (
     <View
       className={`bg-white dark:bg-black/10 rounded-xl p-2 shadow shadow-black/5 ${className} `}
+      {...rest}
     >
       {children}
     </View>
   );
 };
 
-export default ThemeCard;
+export default ThemedCard;
