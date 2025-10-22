34943a6a106236af39e30f7e4bf42569c97cf59c
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-21 20:45:01 -0500

    Carrusel controlado - Funcionalidad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout() {
               name={route.name}
               options={{
                 title: route.title,
+                headerShown: !route.title.includes("Slides"),
               }}
             />
           ))}
